4f3000a68ddcd5453e789f317aaeb033eb1dc0aa
Felipe Santiago <devfelipesantiago@gmail.com>
2024-11-13 16:50:30 -0300

    Release 0.2.7 Update app

--- src/app.js
+++ src/app.js
@@ -1,23 +1,35 @@
+require('dotenv').config();
 const express = require('express');
-const bodyParser = require("body-parser");
-const createUserService = require('./service/createUserService');
-const createUserController = require('./controllers/UserController');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpecs = require('./config/swagger');
+const createUrlService = require('./services/urlService');
+const createUserService = require('./services/userService');
+const createUrlController = require('./controllers/urlController');
+const createUserController = require('./controllers/userController');
+const createUrlRoutes = require('./routes/urlRoutes');
 const createUserRoutes = require('./routes/userRoutes');
-const createUserModel = require('./models/userModel');
+const logger = require('./config/logger');
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
-const secretKey = 'mysecretkey';
+const urlService = createUrlService();
+const userService = createUserService(process.env.JWT_SECRET);
 
-const userModel = createUserModel();
-const userService = createUserService(userModel, secretKey);
+const urlController = createUrlController(urlService);
 const userController = createUserController(userService);
+
+const urlRoutes = createUrlRoutes(urlController);
 const userRoutes = createUserRoutes(userController);
 
-app.use('/api', userRoutes);
+app.use('/api/urls', urlRoutes);
+app.use('/api/users', userRoutes);
+
+app.use((err, req, res, next) => {
+  logger.error('Unhandled error', { error: err.message, stack: err.stack });
+  res.status(500).json({ message: 'An unexpected error occurred' });
+});
 
 module.exports = app;

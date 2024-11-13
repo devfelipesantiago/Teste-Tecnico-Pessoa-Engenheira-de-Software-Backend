8edbe0feae17f29063aaf4a50bff2e0906cb0850
Felipe Santiago <devfelipesantiago@gmail.com>
2024-11-13 16:48:26 -0300

    Release 0.2.4 create user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,13 +1,76 @@
 const express = require('express');
+const validate = require('../middlewares/validate');
+const { userSchema } = require('../validations/userValidations');
 
 const createUserRoutes = (userController) => {
   const router = express.Router();
 
-  router.post('/register', userController.register);
-  router.post('/login', userController.login);
-  router.get('/users', userController.getUsers);
+  /**
+   * @swagger
+   * /api/users/register:
+   *   post:
+   *     summary: Register a new user
+   *     tags: [Users]
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             required:
+   *               - username
+   *               - password
+   *             properties:
+   *               username:
+   *                 type: string
+   *               password:
+   *                 type: string
+   *     responses:
+   *       201:
+   *         description: User registered successfully
+   *       400:
+   *         description: Username already exists
+   */
+  router.post('/register', validate(userSchema), userController.register);
+
+  /**
+   * @swagger
+   * /api/users/login:
+   *   post:
+   *     summary: Login a user
+   *     tags: [Users]
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             required:
+   *               - username
+   *               - password
+   *             properties:
+   *               username:
+   *                 type: string
+   *               password:
+   *                 type: string
+   *     responses:
+   *       200:
+   *         description: Login successful
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 token:
+   *                   type: string
+   *       401:
+   *         description: Invalid credentials
+   */
+  router.post('/login', validate(userSchema), userController.login);
 
   return router;
 };
 
 module.exports = createUserRoutes;
+
+console.log('User routes created with Swagger documentation');
